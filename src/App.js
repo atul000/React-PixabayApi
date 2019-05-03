31ec8a69aab31d753c0058dab0b438a0e7f3ccf2
Atul Bahniwal <atulbahniwal@Atuls-MacBook-Air.local>
2019-05-04 02:14:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './component/navbar/Navbar';
+import Search from './component/search/Search';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MuiThemeProvider>
+    <div>
+
+      <Navbar/><br/>
+      <Search/>
     </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/component/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar';
+
+const Navbar = () => {
+  return (
+    <div>
+      <AppBar title="PixaBay Image Finder" />
+    </div>
+  )
+}
+export default Navbar;
