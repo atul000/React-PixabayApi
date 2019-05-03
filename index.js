83e99bb8494543bc637eb5ed593b5e5b00d6fd5d
Atul Bahniwal <atulbahniwal@Atuls-MacBook-Air.local>
2019-05-04 02:59:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './web/src/App';
+import * as serviceWorker from './web/src/serviceWorker';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 
