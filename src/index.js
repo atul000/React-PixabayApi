00717693431f6d378b1af44c39d654570ef53630
Atul Bahniwal <atulbahniwal@Atuls-MacBook-Air.local>
2019-05-04 03:16:08 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
