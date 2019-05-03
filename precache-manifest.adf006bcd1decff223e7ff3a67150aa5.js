c69eebe9e6d584cdff08dd0fd15c37f7cba2748b
Atul Bahniwal <atulbahniwal@Atuls-MacBook-Air.local>
2019-05-04 03:18:45 +0530

    Updates

---
+++ precache-manifest.adf006bcd1decff223e7ff3a67150aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4f2541c2bdf9dc4da9a68c07c88630",
+    "url": "/React-PixabayApi/index.html"
+  },
+  {
+    "revision": "d80caba0d4f666987235",
+    "url": "/React-PixabayApi/static/css/main.608bd093.chunk.css"
+  },
+  {
+    "revision": "9901335d562ed5e0eb41",
+    "url": "/React-PixabayApi/static/js/2.632be006.chunk.js"
+  },
+  {
+    "revision": "d80caba0d4f666987235",
+    "url": "/React-PixabayApi/static/js/main.949d6ad6.chunk.js"
+  },
+  {
+    "revision": "0c727bca37d07b23f890",
+    "url": "/React-PixabayApi/static/js/runtime~main.580d29dd.js"
+  }
+]);
